5416f82bb74f1dee72ca31a6a2fb404dd5ed89b3
Florian_Striebel <florian.striebel@live.fr>
2020-05-04 17:14:29 +0200

    correctif adresse+ affichage résultat backend

--- back-end/app/api/themes/index.js
+++ back-end/app/api/themes/index.js
@@ -19,6 +19,7 @@ router.get('/', (req, res) => {
 router.get('/:themeName/quizzes', (req, res) => {
   try {
     const response = getQuizzesByTheme(req.params.themeName);
+    console.log(response)
     res.status(200).json(response);
   } catch (err) {
     res.status(500).json(err)

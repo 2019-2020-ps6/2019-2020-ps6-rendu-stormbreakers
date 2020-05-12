1128ac6eb448a06e9f55b58258a6d83551e36d44
Florian_Striebel <florian.striebel@live.fr>
2020-05-12 08:50:53 +0200

    correctif dashboard

--- front-end/src/app/admin/ad-dashboard-default/ad-dashboard-default.component.ts
+++ front-end/src/app/admin/ad-dashboard-default/ad-dashboard-default.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { QuizService } from 'src/services/quiz.service';
+import { MatDialog, MatDialogConfig } from '@angular/material';
+import { Quiz } from 'src/models/quiz.model';
+import { AdminDialogQuizCreateComponent } from 'src/app/admin-dialog-quiz-create/admin-dialog-quiz-create.component';
 
 @Component({
   selector: 'app-ad-dashboard-default',
@@ -7,9 +11,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdDashboardDefaultComponent implements OnInit {
 
-  constructor() { }
+  quizToDispaly:Quiz;
+  constructor(
+    private quizService:QuizService,
+    private dialog:MatDialog) { 
+      quizService.quizzes$.subscribe((quizzes)=>this.quizToDispaly=quizzes[0])
+    }
 
   ngOnInit() {
+    
+    this.quizService.getQuizzes();
   }
+
+  create(quiz:Quiz){
+    this.quizService.addQuiz(quiz)
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.disableClose = false;
+    dialogConfig.autoFocus = true;
+    dialogConfig.data = quiz;
+    const alertdialog =this.dialog.open(AdminDialogQuizCreateComponent,dialogConfig)
+}
+
 
 }
